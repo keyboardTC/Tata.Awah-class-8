2022b65e9367b9ebd1cb117024797a29974f2fbc
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2021-03-21 16:58:39 -0400

    Add , capture operation and check if its valid.

--- calc.sh
+++ calc.sh
@@ -13,6 +13,20 @@ echo "Please select Operation below (* It's Case Sensitive ) ?
     Subtract
     Exit"  
 # Then, capture the user selection.
-# If the selection matches a supported operation, execute the operation.
+while true; do
+# If the selection matches a supported operation, execute the operation
+    read command
+    if [[ $command == "exit" || $command == "Exit" ]]; then
+    	break
+    fi
+.    
 # If the selection does not match a support operation, display an error message.
-# When the operation is complete, redisplay the menu.
+    while [[ "$command" != "Add" ]] && [[ "$command" != "Subtract" ]] && [[ "$command" != "Exit" ]]; do
+    	echo "PLease select a valid operation below (* It's Case Sensitive )
+    	 Add
+    	 Subtract
+    	 Exit"
+    	 read command
+    done
+
+done
